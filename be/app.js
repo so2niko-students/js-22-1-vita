8cdbe37e765ff26e127144e3d29e8b280c85b14a
Veronika Soldatova <113257762+VeronikaSoldatova09@users.noreply.github.com>
2023-09-27 10:31:30 +0200

    Edit cors2

--- be/app.js
+++ be/app.js
@@ -1,45 +1,11 @@
-// const express = require('express')
-// const app = express()
-// const cors = require('cors');
-
-// app.use(cors());
-
-// app.get('/hello', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// module.exports = app;
-
-// const express = require('express')
-// const app = express()
-// const cors = require('cors');
-
-// const corsOptions = {
-//   origin: 'http://example.com',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-
-// app.use(cors(corsOptions));
-
-// app.get('/hello', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// module.exports = app;
-
 const express = require('express')
 const app = express()
 const cors = require('cors');
 
-const corsOptions = {
-  origin: 'http://example.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+app.use(cors());
 
-app.get('/hello', cors(corsOptions),(req, res) => {
-  res.send('Hello World!');
-});
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
 
 module.exports = app;
-
-
