b09ab5da6ef1075097676682b70dd28e3e7c8252
Veronika Soldatova <113257762+VeronikaSoldatova09@users.noreply.github.com>
2023-09-27 10:14:20 +0200

    EDIT CORS

--- be/app.js
+++ be/app.js
@@ -1,8 +1,45 @@
+// const express = require('express')
+// const app = express()
+// const cors = require('cors');
+
+// app.use(cors());
+
+// app.get('/hello', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// module.exports = app;
+
+// const express = require('express')
+// const app = express()
+// const cors = require('cors');
+
+// const corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+
+// app.use(cors(corsOptions));
+
+// app.get('/hello', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// module.exports = app;
+
 const express = require('express')
 const app = express()
+const cors = require('cors');
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
-})
+const corsOptions = {
+  origin: 'http://example.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.get('/hello', cors(corsOptions),(req, res) => {
+  res.send('Hello World!');
+});
 
 module.exports = app;
+
+
