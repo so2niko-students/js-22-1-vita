e3bf519508768d09582300ffce070848c1326636
Veronika Soldatova <113257762+VeronikaSoldatova09@users.noreply.github.com>
2023-09-29 10:50:41 +0200

    Edit be

--- be/app.js
+++ be/app.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const dayjs = require('dayjs');
 
 app.use(cors());
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/date', (req, res) => {
+  const currentDate = dayjs().format('YYYY-MM-DD');
+  res.send(currentDate);
+})
+
+app.get('/time', (req, res) => {
+  const currentTime = dayjs().format('HH:mm:ss');
+  res.send(currentTime);
+})
+
+app.get('/datetime', (req, res) => {
+  const currentDateTime = dayjs().format('YYYY-MM-DD HH:mm:ss');
+  res.send(currentDateTime);
+});
 
 module.exports = app;
