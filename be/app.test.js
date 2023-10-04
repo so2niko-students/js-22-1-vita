2ff512da21fa5ebd162a07def5832b1d0bee738f
Veronika Soldatova <113257762+VeronikaSoldatova09@users.noreply.github.com>
2023-10-04 09:28:55 +0200

    Edit test

--- be/app.test.js
+++ be/app.test.js
@@ -6,5 +6,14 @@ describe('Express App', () => {
         const resp = await request(app).get('/hello');
         expect(resp.status).toEqual(200);
         expect(resp.text).toEqual('Hello World!');
-    })
+    });
+
+    test('/date should return the current date', async () => {
+        const resp = await request(app).get('/date');
+        expect(resp.status).toEqual(200);
+        expect(resp.text).toMatch(/^\d{4}-\d{2}-\d{2}$/);
+    });
 })
+
+
+
